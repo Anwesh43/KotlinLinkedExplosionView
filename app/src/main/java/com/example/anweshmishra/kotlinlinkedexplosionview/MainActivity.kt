183cd1c84b5a06c2932103b17a59e6c409f8902e
Anwesh43 <anweshthecool0@gmail.com>
2018-05-29 03:05:30 +0530

    adding view in activity

--- app/src/main/java/com/example/anweshmishra/kotlinlinkedexplosionview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinlinkedexplosionview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.anweshmishra.kotlinlinkedexplosionview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.linkedexplosionview.LinkedExplosionView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedExplosionView.create(this)
     }
 }
